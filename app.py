3952296dc91daa72f6000b81639177db0cbc6f87
sourav kumar <souravkumar152004@gmail.com>
2026-03-29 10:24:47 +0530

    fixed double app.run issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -28,5 +29,4 @@ def predict():
     return result
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
